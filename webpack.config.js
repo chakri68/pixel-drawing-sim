97750a40d2e3aeb698af40126b3a865c51a73002
chakri68 <chakridevireddy69@gmail.com>
2023-09-06 20:29:52 +0530

    Base files init commit
    - Added favicons and meta tags

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "none",
+  entry: "./src/index.ts",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      filename: "index.html",
+      template: "./src/index.html",
+      title: "Line Drawing Algo Sim",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./src/public", to: "public" }],
+    }),
+  ],
+  devtool: "eval-source-map",
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    hot: true,
+  },
+  resolve: {
+    extensions: ["*", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(s(a|c)ss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_module/,
+        use: "ts-loader",
+      },
+    ],
+  },
+};
