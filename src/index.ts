e52eb4895d1cca80714611c9d18e4a345a12d31a
chakri68 <chakridevireddy69@gmail.com>
2023-09-07 12:45:16 +0530

    base layout

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-import "./styles/global.css";
+import "./styles/global.scss";
 import "./styles/sass/styles.scss";
 
 console.log("index.js connected!");
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d")!;
+
+// window.addEventListener("resize", () => {
+//   const boundingBox = canvas.getBoundingClientRect();
+//   canvas.width = boundingBox.width;
+//   canvas.height = boundingBox.height;
+// });
